e5f5ad4e2b92a458409e979dee30f473708c3936
mppatterson84 <mppatterson84@gmail.com>
2019-03-07 11:40:42 -0600

    Update cover.php
    changed cover headder size

--- template-parts/cover.php
+++ template-parts/cover.php
@@ -1,6 +1,6 @@
     <div class="jumbotron jumbotron-fluid">
         <div class="container text-center headline">
-            <h1 class="display-3">Bootstrap 4</h1>
+            <h1 class="display-4">Bootstrap 4</h1>
             <p class="lead"><?php bloginfo( 'description' ); ?></p>
             <br>
             <hr class="my-3">
