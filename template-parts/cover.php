89ed4a5083f011c763f1cf6154aa8ded5d9078aa
mppatterson84 <mppatterson84@gmail.com>
2019-03-07 18:27:14 -0600

    updated box-shadow class

--- template-parts/cover.php
+++ template-parts/cover.php
@@ -7,11 +7,11 @@
             <br>
             <p class="lead">Wordpress and Bootstrap 4 theme with a front page jumbotron cover.</p>
             <p>
-                <a href="https://github.com/mppatterson84/mikes-bootstrap-4-wordpress-theme" target=_blank class="btn btn-success">GitHub</a>
+                <a href="https://github.com/mppatterson84/mikes-bootstrap-4-wordpress-theme" target=_blank class="btn btn-success button-shadow">GitHub</a>
             </p>
             <br>
             <p class="lead">
-                <a class="btn btn-primary btn-lg animated pulse slower infinite" href="about" role="button">Find Out More</a>
+                <a class="btn btn-primary btn-lg animated pulse slower infinite box-shadow" href="about" role="button">Find Out More</a>
             </p>
         </div>        
     </div>
