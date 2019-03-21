4ba8875180d41b0402b2999331ca604c907eeb54
mppatterson84 <mppatterson84@gmail.com>
2019-03-21 11:38:06 -0500

    Update nav customization

--- header.php
+++ header.php
@@ -19,40 +19,8 @@
 <body <?php body_class(); ?>>
 
 
-    <nav id="mainNavbar" class="navbar navbar-expand-md fixed-top navbar-light nav-pills">
-        <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
-            <?php if( get_theme_mod( 'navbar_brand_display', 'show' ) == 'show' ) : ?>
-                <?php bloginfo('name'); ?>
-            <?php endif ?>
-        </a>
-        <?php if( get_theme_mod( 'navbar_tagline_display', 'show' ) == 'show' ) : ?>
-            <div><?php bloginfo( 'description' ); ?></div>
-        <?php endif ?>
-        <button class="navbar-toggler hamburger hamburger--slider animated rubberBand box-shadow" type="button"
-            data-toggle="collapse" data-target="#bs4navbar"
-            aria-controls="bs4navbar" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="hamburger-box">
-                <span class="hamburger-inner"></span>
-            </span>
-        </button>
-
-        <?php
-            wp_nav_menu(
-                array(
-                    'menu'            => 'top-menu',
-                    'theme_location'  => 'top-menu',
-                    'container'       => 'div',
-                    'container_id'    => 'bs4navbar',
-                    'container_class' => 'collapse navbar-collapse top-nav',
-                    'menu_id'         => 'primary-menu',
-                    'menu_class'      => 'navbar-nav ml-auto text-center animated pulse',
-                    'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                    'depth'           => 2,
-                    'fallback_cb'     => 'bs4navwalker::fallback',
-                    'walker'          => new bs4navwalker()
-                )
-            );
-        ?>
-
-    </nav>
+<?php if( get_theme_mod( 'nav_menu_style', 'style_1' ) == 'style_1' ) : ?>
+    <?php get_template_part( 'template-parts/navs/nav1' ); ?>
+<?php elseif( get_theme_mod( 'nav_menu_style', 'style_2' ) == 'style_2' ) : ?>
+    <?php get_template_part( 'template-parts/navs/nav2' ); ?>
+<?php endif ?>
